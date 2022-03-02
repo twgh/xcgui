a431f6b2710b7a012f453a762fb5d0c3f3559bad
twgh <twgh521@qq.com>
2022-03-02 17:24:57 +0800

    Update objectbase.go

--- objectbase/objectbase.go
+++ objectbase/objectbase.go
@@ -1,7 +1,10 @@
 // 基类.
 package objectbase
 
-import "github.com/twgh/xcgui/xc"
+import (
+	"github.com/twgh/xcgui/xc"
+	"github.com/twgh/xcgui/xcc"
+)
 
 // 炫彩对象基类.
 type ObjectBase struct {
@@ -14,7 +17,7 @@ func (o *ObjectBase) SetHandle(handle int) {
 }
 
 // 炫彩对象_取类型, 获取对象最终类型, 返回对象类型: XC_.
-func (o *ObjectBase) GetType() int {
+func (o *ObjectBase) GetType() xcc.XC_OBJECT_TYPE {
 	return xc.XObj_GetType(o.Handle)
 }
 
@@ -24,13 +27,13 @@ func (o *ObjectBase) GetTypeBase() int {
 }
 
 // 炫彩对象_取类型扩展, 获取对象扩展类型, 返回对象扩展类型: button_type_ , element_type_ , xc_ex_error.
-func (o *ObjectBase) GetTypeEx() int {
+func (o *ObjectBase) GetTypeEx() xcc.XC_OBJECT_TYPE_EX {
 	return xc.XObj_GetTypeEx(o.Handle)
 }
 
 // 炫彩对象_置类型扩展, 如果是按钮, 请使用按钮的增强接口 XBtn_SetTypeEx().
 //
 // nType: 对象扩展类型: button_type_ , element_type_ , xc_ex_error.
-func (o *ObjectBase) SetTypeEx(nType int) int {
+func (o *ObjectBase) SetTypeEx(nType xcc.XC_OBJECT_TYPE_EX) int {
 	return xc.XObj_SetTypeEx(o.Handle, nType)
 }
