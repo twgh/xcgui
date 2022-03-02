208d8d138b417d964c73bb80cbf39c207634a911
twgh <twgh521@qq.com>
2022-03-02 17:24:10 +0800

    Update anima_scale.go

--- ani/anima_scale.go
+++ ani/anima_scale.go
@@ -3,6 +3,7 @@ package ani
 import (
 	"github.com/twgh/xcgui/objectbase"
 	"github.com/twgh/xcgui/xc"
+	"github.com/twgh/xcgui/xcc"
 )
 
 // 动画缩放项.
@@ -12,7 +13,7 @@ type AnimaScale struct {
 
 // 动画缩放_置延伸位置, 设置缩放起点, 确定延伸方向.
 //
-// position: 位置.
-func (a *AnimaScale) SetPosition(position int) bool {
+// position: 位置, Position_Flag_.
+func (a *AnimaScale) SetPosition(position xcc.Position_Flag_) bool {
 	return xc.XAnimaScale_SetPosition(a.Handle, position)
 }
