c18e4016f13bb22735b889e856e8a69c2895cfbe
twgh <twgh521@qq.com>
2022-05-14 06:56:31 +0800

    Create doc.go
    封装了所有窗口组件

---
+++ widget/doc.go
@@ -0,0 +1,6 @@
+// Package widget 封装了所有窗口组件.
+//	Button, ComboBox, DateTime, Edit, Editor, Element, LayoutEle, LayoutFrame, List, ListBox, ListView,
+//	Menu, MenuBar, MonthCal, Pane, ProgressBar, ScrollBar, ScrollView, Shape, ShapeEllipse, ShapeGif,
+//	ShapeGroupBox, ShapeLine, ShapePicture, ShapeRect, ShapeText, SliderBar, TabBar, Table, TextLink,
+//	ToolBar, Tree.
+package widget
