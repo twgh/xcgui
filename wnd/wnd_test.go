df5e101f1848c3a2887b89d57515e16fcd896e1e
twgh <twgh521@qq.com>
2022-05-20 21:33:50 +0800

    Update wnd_test.go

--- wnd/wnd_test.go
+++ wnd/wnd_test.go
@@ -1,20 +1,21 @@
-package wnd
+package wnd_test
 
 import (
 	"github.com/twgh/xcgui/wapi"
+	"github.com/twgh/xcgui/wnd"
 	"testing"
 )
 
 func TestGetWindowTitle(t *testing.T) {
 	hwnd := wapi.FindWindowExW(0, 0, "", "任务管理器")
-	t.Log(GetTitle(hwnd))
+	t.Log(wnd.GetTitle(hwnd))
 }
 
 func TestSetTop(t *testing.T) {
 	hwnd := wapi.FindWindowExW(0, 0, "", "任务管理器")
-	t.Log(SetTop(hwnd, true))
+	t.Log(wnd.SetTop(hwnd, true))
 }
 
 func TestGetWindowHWND(t *testing.T) {
-	t.Log(GetHWND("", "管理器"))
+	t.Log(wnd.GetHWND("", "管理器"))
 }
