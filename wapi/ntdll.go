3983daf466dc6b133544d7855d4e403bd2f2310a
twgh <twgh521@qq.com>
2022-11-22 16:14:41 +0800

    Update ntdll.go

--- wapi/ntdll.go
+++ wapi/ntdll.go
@@ -11,11 +11,11 @@ var (
 )
 
 // RtlMoveMemory 将源内存块的内容复制到目标内存块，并支持重叠的源内存块和目标内存块.
+//
 //	@Description 详情: https://docs.microsoft.com/zh-cn/windows/win32/devnotes/rtlmovememory.
 //	@param Destination 指向要复制字节的目标内存块的指针.
 //	@param Source 指向要复制字节的源内存块的指针.
 //	@param Length 从源复制到目标中的字节数.
-//
 func RtlMoveMemory(Destination uintptr, Source uintptr, Length uint64) {
 	rtlMoveMemory.Call(Destination, Source, uintptr(Length))
 }
