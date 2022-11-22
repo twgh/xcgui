01c5ada574679c4dffd210d80ac5e3c643a04c1e
twgh <twgh521@qq.com>
2022-11-22 16:15:27 +0800

    Update doc.go

--- widget/doc.go
+++ widget/doc.go
@@ -1,4 +1,5 @@
 // Package widget 封装了所有窗口组件.
+//
 //	Button, ComboBox, DateTime, Edit, Editor, Element, LayoutEle, LayoutFrame, List, ListBox, ListView,
 //	Menu, MenuBar, MonthCal, Pane, ProgressBar, ScrollBar, ScrollView, Shape, ShapeEllipse, ShapeGif,
 //	ShapeGroupBox, ShapeLine, ShapePicture, ShapeRect, ShapeText, SliderBar, TabBar, Table, TextLink,
