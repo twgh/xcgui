1d0706e57799bbbe9792198c60c1b648dbb6923a
twgh <twgh521@qq.com>
2023-05-30 00:38:20 +0800

    Create ease_test.go

---
+++ ease/ease_test.go
@@ -0,0 +1,29 @@
+package ease_test
+
+import (
+	"github.com/twgh/xcgui/app"
+	"github.com/twgh/xcgui/ease"
+	"github.com/twgh/xcgui/tf"
+	"github.com/twgh/xcgui/window"
+	"github.com/twgh/xcgui/xc"
+	"github.com/twgh/xcgui/xcc"
+	"testing"
+	"time"
+)
+
+func TestEx(t *testing.T) {
+	tf.TFunc(func(a *app.App, w *window.Window) {
+		time.AfterFunc(time.Millisecond*3, func() {
+			// 获取窗口坐标
+			var rect xc.RECT
+			w.GetRect(&rect)
+
+			for i := 0; i < 30; i++ {
+				v := ease.Ex(float32(i)/30.0, xcc.Ease_Flag_Back|xcc.Ease_Flag_Out)
+				y := int(v * float32(rect.Top))
+				w.SetPosition(int(rect.Left), y)
+				time.Sleep(time.Millisecond * 10)
+			}
+		})
+	})
+}
