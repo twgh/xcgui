6171bc6c1e281e8f9a949f5640444f3db94e8a65
twgh <twgh521@qq.com>
2025-05-20 22:02:19 +0800

    实现 ICoreWebView2_22

--- edge/ICoreWebView2_22.go
+++ edge/ICoreWebView2_22.go
@@ -1,5 +1,14 @@
 package edge
 
+// ok
+
+import (
+	"errors"
+	"golang.org/x/sys/windows"
+	"syscall"
+	"unsafe"
+)
+
 // ICoreWebView2_22 是 ICoreWebView2_21 接口的延续，允许设置筛选器，以便接收服务工作线程、共享工作线程和不同源 iframe 的 已请求web资源 事件。
 //
 // https://learn.microsoft.com/zh-cn/microsoft-edge/webview2/reference/win32/icorewebview2_22
@@ -13,7 +22,60 @@ type ICoreWebView2_22Vtbl struct {
 	RemoveWebResourceRequestedFilterWithRequestSourceKinds ComProc
 }
 
-/*TODO:
-AddWebResourceRequestedFilterWithRequestSourceKinds
-RemoveWebResourceRequestedFilterWithRequestSourceKinds
-*/
+// AddWebResourceRequestedFilterWithRequestSourceKinds 添加带有请求源类型的 Web 资源请求过滤器.
+//
+// uri: 要过滤的 URI.
+//
+// ResourceContext: 资源上下文类型.
+//
+// requestSourceKinds: 请求源类型.
+//
+// https://learn.microsoft.com/zh-cn/microsoft-edge/webview2/reference/win32/icorewebview2_22
+func (i *ICoreWebView2_22) AddWebResourceRequestedFilterWithRequestSourceKinds(uri string, ResourceContext COREWEBVIEW2_WEB_RESOURCE_CONTEXT, requestSourceKinds COREWEBVIEW2_WEB_RESOURCE_REQUEST_SOURCE_KINDS) error {
+	_uri, err := windows.UTF16PtrFromString(uri)
+	if err != nil {
+		return err
+	}
+
+	r, _, err := i.Vtbl.AddWebResourceRequestedFilterWithRequestSourceKinds.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(_uri)),
+		uintptr(ResourceContext),
+		uintptr(requestSourceKinds),
+	)
+	if !errors.Is(err, windows.ERROR_SUCCESS) {
+		return err
+	}
+	if r != 0 {
+		return syscall.Errno(r)
+	}
+	return nil
+}
+
+// RemoveWebResourceRequestedFilterWithRequestSourceKinds 移除带有请求源类型的 Web 资源请求过滤器.
+//
+// uri: 要移除的 URI.
+//
+// ResourceContext: 资源上下文类型.
+//
+// requestSourceKinds: 请求源类型.
+func (i *ICoreWebView2_22) RemoveWebResourceRequestedFilterWithRequestSourceKinds(uri string, ResourceContext COREWEBVIEW2_WEB_RESOURCE_CONTEXT, requestSourceKinds COREWEBVIEW2_WEB_RESOURCE_REQUEST_SOURCE_KINDS) error {
+	_uri, err := windows.UTF16PtrFromString(uri)
+	if err != nil {
+		return err
+	}
+
+	r, _, err := i.Vtbl.RemoveWebResourceRequestedFilterWithRequestSourceKinds.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(_uri)),
+		uintptr(ResourceContext),
+		uintptr(requestSourceKinds),
+	)
+	if !errors.Is(err, windows.ERROR_SUCCESS) {
+		return err
+	}
+	if r != 0 {
+		return syscall.Errno(r)
+	}
+	return nil
+}
