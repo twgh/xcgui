1a793090366b6b1ac317d60a98147e8fd21a1393
twgh <twgh521@qq.com>
2026-05-14 19:29:36 +0800

    增加 GetTaskbarRect

--- wapi/wutil/common.go
+++ wapi/wutil/common.go
@@ -1,9 +1,11 @@
 package wutil
 
 import (
+	"errors"
 	"syscall"
 
 	"github.com/twgh/xcgui/wapi"
+	"github.com/twgh/xcgui/xc"
 )
 
 // GetDropFiles 获取拖放进来的文件.
@@ -23,4 +25,23 @@ func GetDropFiles(hDropInfo uintptr) []string {
 	}
 	wapi.DragFinish(hDropInfo)
 	return files
+}
+
+// GetTaskbarRect 获取任务栏矩形.
+func GetTaskbarRect() (xc.RECT, error) {
+	var rc xc.RECT
+	// 1. 查找任务栏窗口句柄
+	// 任务栏的类名固定为 "Shell_TrayWnd"
+	hwnd := wapi.FindWindowW("Shell_TrayWnd", "")
+	if hwnd == 0 {
+		return rc, errors.New("未找到任务栏窗口")
+	}
+
+	// 2. 获取窗口矩形区域
+	// GetWindowRect 返回的是屏幕坐标
+	if ok := wapi.GetWindowRect(hwnd, &rc); !ok {
+		return rc, errors.New("获取窗口矩形区域失败")
+	}
+
+	return rc, nil
 }
--- wapi/wutil/common_test.go
+++ wapi/wutil/common_test.go
@@ -2,11 +2,12 @@ package wutil_test
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/tf"
 	"github.com/twgh/xcgui/wapi/wutil"
 	"github.com/twgh/xcgui/window"
-	"testing"
 )
 
 func TestGetDropFiles(t *testing.T) {
@@ -17,4 +18,14 @@ func TestGetDropFiles(t *testing.T) {
 			return 0
 		})
 	})
+}
+
+func TestGetTaskbarRect(t *testing.T) {
+	rc, err := wutil.GetTaskbarRect()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("矩形:", rc)
+	fmt.Println("宽度:", rc.Right-rc.Left)
+	fmt.Println("高度:", rc.Bottom-rc.Top)
 }
