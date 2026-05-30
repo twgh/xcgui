32b508ea2370ab8b263c702046fe66b1452141ba
twgh <twgh521@qq.com>
2026-05-30 10:27:13 +0800

    增加 DROPEFFECT_* 常量

--- wapi/consts.go
+++ wapi/consts.go
@@ -21,3 +21,14 @@ const (
 	E_FAIL             syscall.Errno = 0x80004005 // 未指定的失败
 	RO_E_CLOSED        syscall.Errno = 0x80000013 // 对象已关闭, 无法再使用
 )
+
+// DROPEFFECT_* 表示有关拖放操作效果的信息。DoDragDrop 函数以及 IDropSource 和 IDropTarget 中的许多方法都会使用此枚举的值。
+// https://learn.microsoft.com/en-us/windows/win32/com/dropeffect-constants
+
+const (
+	DROPEFFECT_NONE   uint32 = 0          // 放置目标无法接收该数据。
+	DROPEFFECT_COPY   uint32 = 1          // 放置操作将生成副本，拖拽源处的原始数据保持不变。
+	DROPEFFECT_MOVE   uint32 = 2          // 拖拽源应移除该数据。
+	DROPEFFECT_LINK   uint32 = 4          // 拖拽源应创建指向原始数据的链接。
+	DROPEFFECT_SCROLL uint32 = 0x80000000 // 目标区域即将开始滚动或正处于滚动状态。此值需与其他值结合使用。
+)
